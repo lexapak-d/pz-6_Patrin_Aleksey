1a2cdfee60921a401773996d4ce512df51ffb401
lexapak-d <125106941+lexapak-d@users.noreply.github.com>
2023-04-14 12:05:57 +0700

    Add files via upload

---
+++ pr20-21/pr20-21/Form1.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace pr20_21
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void OpenFileDialog(object sender, EventArgs e)
+        {
+            OpenFileDialog OpenDlg = new OpenFileDialog();
+            if (OpenDlg.ShowDialog() == DialogResult.OK)
+            {
+                StreamReader Reader = new StreamReader(OpenDlg.FileName, Encoding.Default);
+                richTextBox1.Text = Reader.ReadToEnd();
+                Reader.Close();
+            }
+            OpenDlg.Dispose();
+
+        }
+
+        private void SaveFileDialog(object sender, EventArgs e)
+        {
+            SaveFileDialog SaveDlg = new SaveFileDialog();
+            if (SaveDlg.ShowDialog() == DialogResult.OK)
+{
+                StreamWriter Writer = new StreamWriter(SaveDlg.FileName);
+                for (int i = 0; i < listBox2.Items.Count; i++)
+                {
+                    Writer.WriteLine((string)listBox2.Items[i]);
+                }
+                Writer.Close();
+            }
+            SaveDlg.Dispose();
+
+        }
+
+        private void ApplicationExit(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            listBox2.Items.Clear();
+            listBox1.BeginUpdate();
+            string[] Strings = richTextBox1.Text.Split(new char[] { '\n', '\t', ' ' },
+            StringSplitOptions.RemoveEmptyEntries);
+            foreach (string s in Strings)
+            {
+                string Str = s.Trim();
+                if (Str == String.Empty) continue;
+                if (radioButton1.Checked) listBox1.Items.Add(Str);
+                if (radioButton2.Checked)
+                {
+                    if (Regex.IsMatch(Str, @"\d")) listBox1.Items.Add(Str);
+                }
+                if (radioButton3.Checked)
+                {
+                    if (Regex.IsMatch(Str, @"\w+@\w+\.\w+")) listBox1.Items.Add(Str);
+                }
+            }
+            listBox1.EndUpdate();
+
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            listBox2.Items.Clear();
+            textBox1.Clear();
+            richTextBox1.Clear();
+            checkBox1.Checked = true;
+            checkBox2.Checked = false;
+            radioButton1.Checked = true;
+            radioButton2.Checked = false;
+            radioButton3.Checked = false;
+
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            listBox2.Items.Clear();
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+            listBox3.Items.Clear();
+            string Find = textBox1.Text;
+            if (checkBox1.Checked)
+            {
+                foreach (string String in listBox1.Items)
+                {
+                    if (String.Contains(Find)) listBox3.Items.Add(String);
+                }
+            }
+
+            if (checkBox1.Checked)
+            {
+                foreach (string String in listBox1.Items)
+                {
+                    if (String.Contains(Find)) listBox3.Items.Add(String);
+                }
+            }
+            if (checkBox2.Checked)
+            {
+                foreach (string String in listBox2.Items)
+                {
+                    if (String.Contains(Find)) listBox3.Items.Add(String);
+                }
+            }
+
+        }
+    }
+}
